cd2d79f1e83df50a9fb2845b7b41f1327c6b94e3
EliMeitner <eli.meitner@toluna.com>
2023-01-10 11:09:57 +0200

    set output

--- output.tf
+++ output.tf
@@ -1,3 +1,5 @@
-output "invoke_arns" {
-    value = aws_lambda_function.init_lambdas
+output "attributes" {
+    value = { for key, value in aws_lambda_function.init_lambdas : key => value }
+    description = "All lambda output parameters"
+    sensitive = true
 }
